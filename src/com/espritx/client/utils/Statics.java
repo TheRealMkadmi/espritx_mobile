c62299bdafb2b6ca324ce8bd973e91b7d9f44dfe
HexCrunch <wahib.mkadmi@esprit.tn>
2022-03-31 00:19:15 +0100

    Fixed base URL

--- src/com/espritx/client/utils/Statics.java
+++ src/com/espritx/client/utils/Statics.java
@@ -10,5 +10,6 @@ package com.espritx.client.utils;
  * @author mouna
  */
 public class Statics {
-    public static final String BASE_URL="https://espritx.xyz/api";
+    //public static final String BASE_URL="https://espritx.xyz/api";
+    public static final String BASE_URL="http://localhost:8000/api";
 }
